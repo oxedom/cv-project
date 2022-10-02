4f931efe6adec470a1b40571d208780c41a01e06
SamLinux <oxedom@gmail.com>
2022-10-02 10:11:01 +0300

    updated company form

--- src/comps/CompanyForm.js
+++ src/comps/CompanyForm.js
@@ -20,17 +20,16 @@ class CompanyForm extends Component {
 
     render() {
         const { jobsData } = this.props
+        const { job, job_title, job_start, job_end } = jobsData
 
-        console.log(jobsData);
         return (<section className="form-box">
             <p className="title"> Work Experince </p>
             <div className="form-inputs">
                 <div className="form-inputs">
-                    <input name="job" defaultValue={`${jobsData.job}`} onChange={this.handleChange} />
-                    <input name="job_title" defaultValue={jobsData.job_title} onChange={this.handleChange} />
-                    <input name="job_start" defaultValue={jobsData.job_start} onChange={this.handleChange} />
-                    <input name="job_end" defaultValue={jobsData.job_end} onChange={this.handleChange} />
-
+                    <input name="job" value={job} onChange={this.handleChange} />
+                    <input name="job_title" value={job_title} onChange={this.handleChange} />
+                    <input name="job_start" value={job_start} onChange={this.handleChange} />
+                    <input name="job_end" value={job_end} onChange={this.handleChange} />
                 </div>
             </div>
             <div onClick={this.handleClick} a-id={jobsData.id} className="btn"> Delete </div>
